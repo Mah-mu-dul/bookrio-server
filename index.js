c3e9def4797f573aae81544ba404bdeb85a1fe9d
habituatedcoder <work.mahmudulhasan@gmail.com>
2022-05-14 07:48:07 +0600

    url currection done

--- index.js
+++ index.js
@@ -28,31 +28,27 @@ async function run() {
 
 
 
-        app.post('http://localhost:5000/myitems', (req, res) =>{
-            const email = req.email
-            console.log(email);
-            
-
-        })
 
 
 
         // function for verify jwt
-        const verifyJWT = (req , res, next) => {
+        const verifyJWT = (req, res, next) => {
             const authHeader = req.headers.authorization
-            if(!authHeader){
-                return res.status(401).send({messege: 'un authorozied access'})
+            if (!authHeader) {
+                return res.status(401).send({ messege: 'un authorozied access dont find header' })
             }
-            const token = authHeader.split(' ')[1]
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECREATE , (err, decoded) =>{
-                if (err){
-                    res.status(403).send({messege: 'forbidden access'})
+            
+            const token = authHeader?.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECREATE, (err, decoded) => {
+                if (err) {
+                    return res.status(403).send({ messege: 'forbidden access' })
                 }
                 req.decoded = decoded
             })
+            next()
 
         }
-        
+
 
 
         // show all items
@@ -78,94 +74,93 @@ async function run() {
         app.get(`/books/:email`, verifyJWT, async (req, res) => {
             const decodedEmail = req.decoded.email
             console.log(decodedEmail)
-          
+
             const email = req.params.email
-            if (email === decodedEmail){
-                 const query = {email}
+
+            const query = { email }
             const cursor = BooksCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
-            }
-            else{
-                res.status(403).send({messege: 'forbidden access'})
-            }
+
+
+        
         
            
 
         })
 
 
-        // post a new item
-        app.post('/books', async (req, res) => {
-            const newBook = req.body
-            console.log('adding new book', newBook);
-            const result = await BooksCollection.insertOne(newBook)
-            res.send(result)
-        })
+    // post a new item
+    app.post('/books', async (req, res) => {
+        const newBook = req.body
+        console.log('adding new book', newBook);
+        const result = await BooksCollection.insertOne(newBook)
+        res.send(result)
+    })
 
-        //update an item
-        app.put('/book/:id', async (req, res) => {
-            const id = req.params.id
-            const updatedItem = req.body
-            const filter = { _id: ObjectId(id) }
-            const options = { upsert: true }
-            const updatedDoc = {
-                $set: {
-                    quantity: updatedItem.quantity
-
-                }
+    //update an item
+    app.put('/book/:id', async (req, res) => {
+        const id = req.params.id
+        const updatedItem = req.body
+        const filter = { _id: ObjectId(id) }
+        const options = { upsert: true }
+        const updatedDoc = {
+            $set: {
+                quantity: updatedItem.quantity
 
             }
-            const result = await BooksCollection.updateOne(filter, updatedDoc, options)
-            res.send(result)
+
+        }
+        const result = await BooksCollection.updateOne(filter, updatedDoc, options)
+        res.send(result)
 
 
-        })
+    })
 
 
-        // delete a items 
+    // delete a items 
 
-        app.delete('/books/:id', async (req, res) => {
-            const id = req.params.id
+    app.delete('/books/:id', async (req, res) => {
+        const id = req.params.id
 
-            const query = { _id: ObjectId(id) }
-            console.log(query)
-            const result = await BooksCollection.deleteOne(query)
-            res.send(result)
-        })
+        const query = { _id: ObjectId(id) }
+        console.log(query)
+        const result = await BooksCollection.deleteOne(query)
+        res.send(result)
+    })
 
-        //post a blog
-        app.post('/blogs', async (req, res) => {
-            const newBlog = req.body
-            console.log('adding new blog', newBlog);
-            const result = await Blogs.insertOne(newBlog)
-            res.send(result)
-        })
+    //post a blog
+    app.post('/blogs', async (req, res) => {
+        const newBlog = req.body
+        console.log('adding new blog', newBlog);
+        const result = await Blogs.insertOne(newBlog)
+        res.send(result)
+    })
 
-        //read a blog
-        app.get('/blogs', async (req, res) => {
-            const query = {}
-            const cursor = Blogs.find(query)
-            const blogs = await cursor.toArray()
-            res.send(blogs)
-        })
-
-
-        // auth (jwt)
-        app.post('/login', async (req, res) => {
-            const user = req.body
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECREATE
-            )
-            res.send({ token })
-        })
+    //read a blog
+    app.get('/blogs', async (req, res) => {
+        const query = {}
+        const cursor = Blogs.find(query)
+        const blogs = await cursor.toArray()
+        res.send(blogs)
+    })
 
 
-        // get items by mail
+    // auth (jwt)
+    app.post('/login', async (req, res) => {
+        const user = req.body
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECREATE
+        )
+        res.send({ token })
+    })
 
-    }
+
+    // get items by mail
+
+}
     finally {
 
-    }
+}
 
 
 }
